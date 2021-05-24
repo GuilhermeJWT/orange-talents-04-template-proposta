878e04b2ec3ad68dc1b2b7de5752b4ee69bd1cd2
GuilhermeJWT <guiromanno@gmail.com>
2021-05-24 09:23:19 -0300

    Adicionando Modelo de Biometria e Repository

---
+++ src/main/java/br/com/zupacademy/guilhermesantos/proposta/model/ModelBiometria.java
@@ -0,0 +1,58 @@
+package br.com.zupacademy.guilhermesantos.proposta.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "biometria")
+public class ModelBiometria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "A Data do Cadastro deve ser Informada!")
+    private LocalDateTime instanteCadastro;
+
+    @NotNull(message = "A Biometria deve ser Informada!")
+    private byte[] fingerPrintBiometria;
+
+    @NotNull(message = "O Cartão deve ser Informado!")
+    @ManyToOne(optional = false)
+    private ModelCartao cartao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getInstanteCadastro() {
+        return instanteCadastro;
+    }
+
+    public byte[] getFingerPrintBiometria() {
+        return fingerPrintBiometria;
+    }
+
+    public ModelCartao getCartao() {
+        return cartao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ModelBiometria)) return false;
+        ModelBiometria that = (ModelBiometria) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
---
+++ src/main/java/br/com/zupacademy/guilhermesantos/proposta/repository/BiometriaRepository.java
@@ -0,0 +1,10 @@
+package br.com.zupacademy.guilhermesantos.proposta.repository;
+
+import br.com.zupacademy.guilhermesantos.proposta.model.ModelBiometria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BiometriaRepository extends JpaRepository<ModelBiometria, Long> {
+    
+}
